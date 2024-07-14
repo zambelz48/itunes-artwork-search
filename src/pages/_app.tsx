a8a6b16e5a6170f0a514839128802b7a0bcdc991
zambelz48 <nanda.julianda48@gmail.com>
2024-07-15 01:45:24 +0700

    chore: remove unnecessary package

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,15 @@
 import type { AppProps } from "next/app"
 import Head from "next/head"
-import { QueryClient, QueryClientProvider } from "react-query"
 
 import "@/styles/globals.css"
 
-const reactQueryClient = new QueryClient()
-
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <QueryClientProvider client={reactQueryClient}>
-      <>
-        <Head>
-          <title>Logique Test</title>
-        </Head>
-        <Component {...pageProps} />
-      </>
-    </QueryClientProvider>
+    <>
+      <Head>
+        <title>Logique Test</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
   )
 }
